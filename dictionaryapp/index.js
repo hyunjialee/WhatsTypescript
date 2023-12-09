da66a0d3738f11e7ab7d0dce86fa7ecc0e2349e0
hyunjialee <hyjialee@gmail.com>
2023-12-08 23:16:25 -0500

    main web app working

---
+++ dictionaryapp/index.js
@@ -0,0 +1,43 @@
+class DictionaryApp {
+    apiUrl
+
+        async searchWord() {
+
+        const wordInput = document.getElementById('wordInput');
+        const resultDiv = document.getElementById('result');
+
+        if (resultDiv) {
+            const word = wordInput.value.trim();
+//            console.log(`${this.apiUrl}${word}`)
+
+            if (word === '') {
+                resultDiv.innerHTML = '<p>Please enter a word.</p>';
+                return;
+            }
+
+            try {
+                // Fetch data from the dictionary API
+                const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+                const data = await response.json();
+
+                // Displays the definition of the word
+                if (Array.isArray(data) && data.length > 0) {
+                    const definition = data[0].meanings[0].definitions[0].definition;
+                    resultDiv.innerHTML = `<p><strong>${word}:</strong> ${definition}</p>`;
+                } else {
+                    resultDiv.innerHTML = '<p>No definition found.</p>';
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                resultDiv.innerHTML = '<p>An error occurred. Please try again later.</p>';
+            }
+        }
+    }
+}
+
+// Initialize the Dictionary App
+document.addEventListener('DOMContentLoaded', () => {
+    const dictionaryApp = new DictionaryApp();
+    const searchButton = document.getElementById('searchButton');
+    searchButton.addEventListener('click', dictionaryApp.searchWord);
+});
